46d847f2976f373df39c45cf96b8f588e63cfead
TelRich <okorogoodrich@gmail.com>
2023-03-19 04:19:45 +0100

    change 2

--- db_test.py
+++ db_test.py
@@ -2,12 +2,29 @@ import streamlit as st
 import pandas as pd
 
 
-username = 'test'
+# username = 'test'
+# password = st.secrets['pw']
+# host = 'telrichserver.postgres.database.azure.com'
+# database = 'newdb'
+
+# conn_str = f'postgresql+psycopg2://{username}:{password}@{host}/{database}'
+
+
+
+from sqlalchemy import create_engine
+
+# Set up a connection string
+username = st.secrets['user']
 password = st.secrets['pw']
 host = 'telrichserver.postgres.database.azure.com'
+# port = 'your_port'
 database = 'newdb'
+conn_str = f'postgresql://{username}:{password}@{host}/{database}'
 
-conn_str = f'postgresql+psycopg2://{username}:{password}@{host}/{database}'
+# Create a connection engine
+engine = create_engine(conn_str)
 
-df = pd.read_sql_query("select * from bus_breakdown_delay limit 5", conn_str)
+# Execute a SQL query
+query = 'SELECT * FROM bus_breakdown_delay LIMIT 5'
+df = pd.read_sql_query(query, engine)
 st.write(df)
