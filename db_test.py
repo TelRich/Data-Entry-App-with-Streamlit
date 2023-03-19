ed229b30bd4049e7a7f105431aaec292f53cf417
TelRich <okorogoodrich@gmail.com>
2023-03-19 12:22:29 +0100

    close db connection

--- db_test.py
+++ db_test.py
@@ -57,6 +57,7 @@ if st.button('Save Details'):
     query = f'''INSERT INTO phone_sales (phone_brand, phone_model, purchase_date, sold_date, sold_price, cost_price)
     VALUES ('{phone_brand}', '{phone_model}', '{purchase_date}', '{sold_date}', '{sold_price}', '{cost_price}')'''
     cur.execute(query)
+    cur.close()
 
 # Execute a SQL query and fetch the results
 query = 'SELECT * FROM phone_sales'
