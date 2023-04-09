82662fde9d030f4664dc4fbf9b9cb9b119f84b77
TelRich <okorogoodrich@gmail.com>
2023-04-09 15:14:44 +0100

    adjusted documentation

--- db_test.py
+++ db_test.py
@@ -219,7 +219,7 @@ with col2.expander('Documentation'):
 
             This documentation provides an overview of a data entry application developed using Streamlit and connected to an Azure Postgres database. 
             The app allows users to enter data into a form and saves the in the database.
-            The app also includes a visualization that changes with respect to the latest entry. In addition, users can download the data from the database and view charts created in Power BI.
+            The app also includes a visualization that changes with respect to the latest entry. In addition, users can download the data from the database. On the other end, Power BI is connected to the same database to generate data visualizations.
 
             >**Project Overview**
 
@@ -232,6 +232,8 @@ with col2.expander('Documentation'):
             * Streamlit
             * Python
             * Azure Postgres Database
+            * Python Plotly
+            * Power BI
 
             >**Functionality**:
 
@@ -243,11 +245,16 @@ with col2.expander('Documentation'):
 
             4. Download Data: The app allows users to download the data from the database. This feature is useful for users who want to perform further analysis on the data using other tools.
 
+            5. Power BI Integration: The app is integrated with Power BI, which allows users to view charts and perform additional analysis on the data. The charts have been designed for the mobile view of Power BI, ensuring that users can access the data on-the-go.
+
             > **Conclusion**:
 
-            The Streamlit data entry app with an Azure Postgres database and Plotly visualization provides a robust and user-friendly solution for data collection and analysis. 
-            The app's interface and dynamic visualization make data entry and analysis quick and efficient, also providing a means to download the data. 
-            Overall, this app provides a comprehensive solution for data collection and analysis needs.
+            The Streamlit data entry app, combined with an Azure Postgres database and Plotly visualization, provides a robust and user-friendly solution for data collection and analysis. The app's interface and dynamic visualization make data entry and analysis quick and efficient, while also providing a way to download the data, and the connection to Power BI allows for more in-depth analysis and visualization. Overall, this app provides a comprehensive solution for data collection and analysis needs.
+
+            > **Future Work**
+
+            * This project can be improved/modified further by directly embedding the Power BI dashboard on the app. 
+            * More data storage is provided, as are additional charts displaying various other insights.
             
              """)
 
